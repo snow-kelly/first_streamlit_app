761ba27fa260fba0aba7bf8abddb8547fbd49ebb
snow-kelly <151071216+snow-kelly@users.noreply.github.com>
2023-11-16 12:42:46 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfase Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('KAle, Spinach & Rocket Smoothie')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
